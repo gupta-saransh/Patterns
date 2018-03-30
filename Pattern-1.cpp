b5dd6c993cee6488e32b2a04d9cb822250b88bf8
Saransh Gupta <35869697+srg3006@users.noreply.github.com>
2018-03-30 23:41:11 +0530

    Add files via upload
    ****
    ****
    ****
    ****
    
    
    For user input 4.

---
+++ Pattern-1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+	
+	int i,n,j;
+	
+		cout<<"Please enter number to print pattern"<<endl;
+		cin>>n;
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		cout<<"*";
+		cout<<endl;
+	}
+	
+	
+	return 0;
+}
